2e89d2f80ec0a61aed39d580ab9625d2400e4621
Adise <admin@pauibanez.me>
2026-01-20 11:07:08 +0100

    added singleton setup

--- Assets/Scripts/ResourceSystem/ResourceManager/ResourceManager.cs
+++ Assets/Scripts/ResourceSystem/ResourceManager/ResourceManager.cs
@@ -6,6 +6,19 @@ namespace ThriveOrDie.ResourceSystem
 {
   public class ResourceManager : MonoBehaviour
   {
+    #region SingletonSetup
+    public static ResourceManager Singleton;
+
+    /// <summary>Sets up the singleton</summary>
+    private void SetupSingleton()
+    {
+      #region SetupSingleton
+      if (Singleton != null && Singleton != this) { Destroy(gameObject); return; }
+      if (Singleton != this) Singleton = this;
+      #endregion
+    }
+    #endregion
+
     #region Data
     /// <summary>The wood resource</summary>
     [SerializeField]
@@ -14,6 +27,18 @@ namespace ThriveOrDie.ResourceSystem
     [SerializeField]
     private Resource scrap = new Resource(ResourceType.Scrap);
     #endregion
+    #region Unity
+    /// <summary>Called by unity on awake</summary>
+    private void Awake()
+    {
+      #region Awake
+      SetupSingleton();
+      #endregion
+    }
+    #endregion
 
+    #region Methods
+
+    #endregion
   }
 }
