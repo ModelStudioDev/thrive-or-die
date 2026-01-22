445ae22206cc55e6b12ddedfab2e600a4ab91033
Adise <admin@pauibanez.me>
2026-01-22 13:41:50 +0100

    added base map generation

--- Assets/Scripts/Map/MapGenerator/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator/MapGenerator.cs
@@ -1,25 +1,85 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class MapGenerator : MonoBehaviour
+namespace ThriveOrDie.Map
 {
-  /// <summary>The ground base tiles</summary>
-  [SerializeField]
-  private TileBase[] groundTiles;
-
-  /// <summary>The ground tilemap</summary>
-  [SerializeField]
-  private Tilemap groundTilemap;
-
-  // Start is called once before the first execution of Update after the MonoBehaviour is created
-  void Start()
+  public class MapGenerator : MonoBehaviour
   {
 
-  }
+    #region Data
+    /// <summary>The ground base tiles</summary>
+    [SerializeField]
+    private TileBase[] groundTiles;
 
-  // Update is called once per frame
-  void Update()
-  {
+    /// <summary>The ground tilemap</summary>
+    [SerializeField]
+    private Tilemap groundTilemap;
 
+    /// <summary>The size of the map</summary>
+    [SerializeField]
+    private int _mapSize;
+    /// <summary>The size of the map</summary>
+    public int mapSize => _mapSize;
+
+    /// <summary>The modifyable map of tiles</summary>
+    private List<List<Map.TileData>> _groundMap;
+
+    /// <summary>READ-ONLY copy of the map</summary>
+    public IReadOnlyList<IReadOnlyList<Map.TileData>> groundMap => GetGroundMap();
+    #endregion
+
+    #region Unity
+    /// <summary>Run by unity</summary>
+    private void Start()
+    {
+      #region Start
+      // TODO: THIS SHOULD NOT BE CALLED FROM HERE BUT FROM THE SAVES IF NOT PRESENT!
+      PopulateMap();
+      #endregion
+    }
+    #endregion
+
+
+    #region Methods
+    /// <summary>Populates the map</summary>
+    public void PopulateMap()
+    {
+      #region PopulateMap
+      for (int row = 0; row < _mapSize; row++)
+      {
+        for (int col = 0; col < _mapSize; col++)
+        {
+          TileBase tileBase = GetTile(row, col);
+          _groundMap[row][col] = new TileData(tileBase);
+          groundTilemap.SetTile(new Vector3Int(row, col), tileBase);
+        }
+      }
+      #endregion
+    }
+
+    /// <summary>Gets a copy of the map as read-only</summary>
+    private IReadOnlyList<IReadOnlyList<TileData>> GetGroundMap()
+    {
+      #region GetGroundMap
+      return _groundMap.Select(row => row.ToArray()).ToArray();
+      #endregion
+    }
+
+
+    /// <summary>Gets a tile base for the passed coordenates</summary>
+    /// <param name="row">The row of the tile</param>
+    /// <param name="col">The col of the tile</param>
+    /// <returns>The tile base asset for the coorrdenates</returns>
+    private TileBase GetTile(int row, int col)
+    {
+      #region GetTile
+      // TODO: Generate depending on coordenates
+
+      return groundTiles[Random.Range(0, groundTiles.Length)];
+      #endregion
+    }
+    #endregion
   }
 }
--- Assets/Scripts/Map/TileData/TileData.cs
+++ Assets/Scripts/Map/TileData/TileData.cs
@@ -11,25 +11,38 @@ namespace ThriveOrDie.Map
     /// <summary>Whether this tile is buildable</summary>
     /// <remarks> Default value: TRUE </remarks>
     public bool isBuildable { get; private set; } = true;
+
     /// <summary>The `TileBase` assset (Sprite) of this tile</summary>
-    public TileBase tileBase;
+    private TileBase _tileBase;
+    /// <summary>The `TileBase` assset (Sprite) of this tile</summary>
+    public TileBase tileBase => _tileBase;
     #endregion
 
     #region Constructor
     /// <summary>Creates a new tile with default values</summary>
     public TileData() { }
-    /// <summary>Creates a new tile with the buildable state as passed</summary>
-    /// <param name="buildable">Whether the tile is buildable</param>
-    public TileData(bool buildable) { isBuildable = buildable; }
+    /// <summary>Creates a new tile with the passed tileBase</summary>
+    /// <param name="tileBase">The tileBase to set</param>
+    public TileData(TileBase tileBase) { _tileBase = tileBase; }
+
     #endregion
 
     #region Methods
-    /// <summary>Sets this tile buildable state.</summary>
-    /// <param name="buildable"></param>
+    /// <summary>Sets this tile buildable state</summary>
+    /// <param name="buildable">Whether the tile is buildable</param>
     public void SetIsBuildableTo(bool buildable)
     {
       #region SetIsBuildableTo
       isBuildable = buildable;
+      #endregion
+    }
+
+    /// <summary>Sets this tile base asset</summary>
+    /// <param name="newTileBase">The new tile base</param>
+    public void SetTileBase(TileBase newTileBase)
+    {
+      #region SetTileBase
+      _tileBase = newTileBase;
       #endregion
     }
     #endregion
