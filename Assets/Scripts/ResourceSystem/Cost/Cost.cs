2aee7dd3a4f301308fcd2a1cf1629447dbaa706e
Adise <admin@pauibanez.me>
2026-01-25 19:59:04 +0100

    ressolved: https://github.com/Adise0/thrive-or-die/pull/1#discussion_r2716581610

--- Assets/Scripts/ResourceSystem/Cost/Cost.cs
+++ Assets/Scripts/ResourceSystem/Cost/Cost.cs
@@ -2,8 +2,10 @@
 
 namespace ThriveOrDie.ResourceSystem
 {
+  /// <summary>The cost wrapper object is a containner for any resource cost</summary>
   public struct Cost
   {
+    /// <summary>The amount of resources</summary>
     public Resource[] costs;
   }
 
