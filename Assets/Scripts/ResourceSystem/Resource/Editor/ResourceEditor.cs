edd2d4f80468b779e10d4ad95c022e7c5d4de73b
Adise <admin@pauibanez.me>
2026-01-25 20:53:51 +0100

    added new editor script and UI and resolved: https://github.com/Adise0/thrive-or-die/pull/1#discussion_r2716582482

--- Assets/Scripts/ResourceSystem/Resource/Editor/ResourceEditor.cs
+++ Assets/Scripts/ResourceSystem/Resource/Editor/ResourceEditor.cs
@@ -1,4 +1,5 @@
 
+using System;
 using ThriveOrDie.ResourceSystem;
 using UnityEditor;
 using UnityEditor.UIElements;
@@ -10,48 +11,94 @@ namespace ThriveOrDie.Editors
   [CustomPropertyDrawer(typeof(Resource))]
   public class ResourceEditor : PropertyDrawer
   {
+    #region Data
+    /// <summary>The underlying visual tree asset</summary>
+    private static VisualTreeAsset visualTreeAsset;
+    #endregion
 
+    #region Unity
+    /// <summary>Called by Unity</summary>
+    /// <param name="property">The Serialized prooperty</param>
+    /// <returns>The created VisualElement</returns>
     public override VisualElement CreatePropertyGUI(SerializedProperty property)
     {
+      #region CreatePropertyGUI
+      return RenderResourceAmount(property);
+      #endregion
+    }
+    #endregion
 
-      SerializedProperty resourceType = property.FindPropertyRelative("_resourceType");
-      SerializedProperty amount = property.FindPropertyRelative("_amount");
+    #region Rendering
+    /// <summary>Renders the resource amount propperty</summary>
+    /// <param name="property">The ResourceAmount serialized property</param>
+    /// <returns>The created and configured VisualElement</returns>
+    private VisualElement RenderResourceAmount(SerializedProperty property)
+    {
+      #region RenderResourceAmount
+      SerializedProperty amountProp = property.FindPropertyRelative("_amount");
 
-      VisualElement root = new VisualElement();
-      root.style.flexDirection = FlexDirection.Column;
+      VisualElement root = GetVisualElement();
+      if (root == null)
+        return new Label("Missing ResourceProperty.uxml");
 
-      Label nameElm = new Label();
-      nameElm.text = resourceType.enumDisplayNames[resourceType.intValue];
-      nameElm.style.unityFontStyleAndWeight = FontStyle.Bold;
-      root.Add(nameElm);
+      Label name = root.Q<Label>("name");
+      Label amount = root.Q<Label>("amount");
+      IntegerField input = root.Q<IntegerField>("input");
 
-      VisualElement valuesHolder = new VisualElement();
-      valuesHolder.style.paddingLeft = 10;
-      valuesHolder.style.flexDirection = FlexDirection.Row;
-      valuesHolder.style.justifyContent = Justify.SpaceBetween;
+      VisualElement defaultHolder = root.Q<VisualElement>("default");
+      VisualElement editHolder = root.Q<VisualElement>("edit");
 
-      root.Add(valuesHolder);
+      Button setBtn = root.Q<Button>("setBtn");
+      Button cancelBtn = root.Q<Button>("cancelBtn");
+      Button confirmBtn = root.Q<Button>("confirmBtn");
 
-      Label amountElm = new Label();
-      amountElm.text = amount.intValue.ToString();
-      valuesHolder.Add(amountElm);
+      name.text = property.displayName;
+      amount.text = amountProp.intValue.ToString();
+      input.SetValueWithoutNotify(amountProp.intValue);
 
 
-      Button setAmountButton = new Button();
-      setAmountButton.text = "Set";
-      valuesHolder.Add(setAmountButton);
 
-      setAmountButton.clicked += () =>
+      Action<bool> toggleEditUI = (bool show) =>
       {
-        Debug.Log("This is not implemented yet!");
+        defaultHolder.style.display = show ? DisplayStyle.None : DisplayStyle.Flex;
+        editHolder.style.display = show ? DisplayStyle.Flex : DisplayStyle.None;
       };
 
-      root.TrackPropertyValue(amount, property =>
-      {
-        amountElm.text = property.intValue.ToString();
-      });
 
+      root.Q<Button>("setBtn").clicked += () => { toggleEditUI(true); input.Focus(); };
+      root.Q<Button>("cancelBtn").clicked += () => { toggleEditUI(false); input.SetValueWithoutNotify(amountProp.intValue); };
+
+      root.Q<Button>("confirmBtn").clicked += () =>
+      {
+        toggleEditUI(false);
+        amountProp.intValue = input.value;
+        input.SetValueWithoutNotify(amountProp.intValue);
+        property.serializedObject.ApplyModifiedProperties();
+      };
+
+      root.TrackPropertyValue(amountProp, t =>
+      {
+        amount.text = amountProp.intValue.ToString();
+        input.SetValueWithoutNotify(amountProp.intValue);
+      });
       return root;
+      #endregion
     }
+    #endregion
+
+    #region Methods
+    /// <summary>Creates the root visual element</summary>
+    /// <returns>The created root visual element</returns>
+    private VisualElement GetVisualElement()
+    {
+      #region GetVisualElement
+      if (visualTreeAsset == null)
+      {
+        visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/ResourceSystem/Resource/Editor/ResourceProperty.uxml");
+      }
+      return visualTreeAsset != null ? visualTreeAsset.CloneTree() : null;
+      #endregion
+    }
+    #endregion
   }
 }
