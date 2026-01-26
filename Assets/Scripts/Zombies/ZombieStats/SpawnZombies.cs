0afccd05b1c3495e8db96338b6c7d6954849f45e
Nil <nil.badia@estudiant.enti.cat>
2026-01-26 10:43:33 +0100

    added sprite arrays to the scriptable object and created a .cs to test spawning

---
+++ Assets/Scripts/Zombies/ZombieStats/SpawnZombies.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace ThriveOrDie.Zombies
+
+{
+  public class SpawnZombies : MonoBehaviour
+  {
+    public GameObject zombie;
+    
+  
+
+    void Start()
+    {
+      SpawnEntities();
+    }
+
+    void SpawnEntities()
+    {
+      for (int i = 0; i < 3; i++)
+      {
+        GameObject newZombie = Instantiate(zombie, new Vector2(0, 0), Quaternion.identity);
+      }
+    
+ 
+    }
+  }
+}
--- Assets/Scripts/Zombies/ZombieStats/ZombieStats.cs
+++ Assets/Scripts/Zombies/ZombieStats/ZombieStats.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 
 namespace ThriveOrDie.Zombies
 {
-  [CreateAssetMenu(fileName = "ZombieStats", menuName = "Scriptable Objects/ZombieStats")]
+  [CreateAssetMenu(fileName = "ZombieStats", menuName = "ScriptableObjects/ZombieStats")]
   public class ZombieStats : ScriptableObject
   {
     public string prefabName;
 
+    public Sprite[] idleSprites;
+    public Sprite[] atackSprites;
+    public Sprite[] walkingSprites;
+    public Sprite[] runningSprites;
+    public Sprite[] deathSprites;
+
     public int numberOfPrefabsToCreate;
     public Vector2[] spawnPoints;
 
-    public ZombieType type;
-    private ZombieLevel level;
+    //public ZombieType type;
+    //public ZombieLevel level;
 
     public float maxHealth;
     public float strength;
