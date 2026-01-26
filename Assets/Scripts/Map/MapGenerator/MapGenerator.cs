ea03d85b0fae79caeb13ef04f6e47392c44c60ae
Adise <admin@pauibanez.me>
2026-01-26 09:32:10 +0100

    converted the MapGenerator to a static class

--- Assets/Scripts/Map/MapGenerator/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator/MapGenerator.cs
@@ -5,45 +5,20 @@ using UnityEngine.Tilemaps;
 
 namespace ThriveOrDie.Map
 {
-  public class MapGenerator : MonoBehaviour
+  /// <summary>Generates the map tiles</summary>
+  public static class MapGenerator
   {
-
     #region Data
-    /// <summary>The ground base tiles</summary>
-    [SerializeField]
-    private TileBase[] groundTiles;
-
-    /// <summary>The ground tilemap</summary>
-    [SerializeField]
-    private Tilemap groundTilemap;
-
-    /// <summary>The size of the map</summary>
-    [SerializeField]
-    private int _mapSize;
-    /// <summary>The size of the map</summary>
-    public int mapSize => _mapSize;
-
     /// <summary>The modifyable map of tiles</summary>
-    private List<List<MapTile>> _groundMap = new List<List<MapTile>>();
+    private static List<List<MapTile>> _groundMap = new List<List<MapTile>>();
 
     /// <summary>READ-ONLY copy of the map</summary>
-    public IReadOnlyList<IReadOnlyList<MapTile>> groundMap => GetGroundMap();
-    #endregion
-
-    #region Unity
-    /// <summary>Run by unity</summary>
-    private void Start()
-    {
-      #region Start
-      // TODO: THIS SHOULD NOT BE CALLED FROM HERE BUT FROM THE SAVES IF NOT PRESENT!
-      PopulateMap();
-      #endregion
-    }
+    public static IReadOnlyList<IReadOnlyList<MapTile>> groundMap => GetGroundMap();
     #endregion
 
     #region Methods
     /// <summary>Populates the map</summary>
-    public void PopulateMap()
+    public static void PopulateMap()
     {
       #region PopulateMap
       int half = _mapSize / 2;
@@ -65,18 +40,17 @@ namespace ThriveOrDie.Map
 
     /// <summary>Gets a copy of the map as read-only</summary>
     /// <returns>The read-only copy of the ground</returns>
-    private IReadOnlyList<IReadOnlyList<MapTile>> GetGroundMap()
+    private static IReadOnlyList<IReadOnlyList<MapTile>> GetGroundMap()
     {
       #region GetGroundMap
       return _groundMap.Select(row => row.ToArray()).ToArray();
       #endregion
     }
 
-
     /// <summary>Gets a tile base for the passed coordenates</summary>
     /// <param name="position">The position of the tile</param>
     /// <returns>The tile base asset for the coorrdenates</returns>
-    private TileBase GetTile(Vector2Int position)
+    private static TileBase GetTile(Vector2Int position)
     {
       #region GetTile
       // TODO: Generate depending on coordenates
