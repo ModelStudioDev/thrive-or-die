fb457ebd032402d34ca023432012edf09309af7c
Adise <admin@pauibanez.me>
2026-01-26 11:43:56 +0100

    fixed typo

--- Assets/Scripts/Map/MapGenerator/MapGeneratorData/MapGeneratorData.cs
+++ Assets/Scripts/Map/MapGenerator/MapGeneratorData/MapGeneratorData.cs
@@ -6,7 +6,6 @@ public class MapGeneratorData : ScriptableObject
 {
   /// <summary>The map size. Consider puting this in the Game manager instead?</summary>
   public int mapSize;
-  /// <summary>the collection of ground tiles to use</summary>
+  /// <summary>The collection of ground tiles to use</summary>
   public TileBase[] groundTiles;
-
 }
