1ef5efb89133043dceadd0bffc4790d9cf2baa10
Adise <admin@pauibanez.me>
2026-01-26 09:58:27 +0100

    populated the ground generator data

--- Assets/Scripts/Map/MapGenerator/MapGeneratorData/MapGeneratorData.cs
+++ Assets/Scripts/Map/MapGenerator/MapGeneratorData/MapGeneratorData.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-[CreateAssetMenu(fileName = "Map Generation Data", menuName = "ThriveOrDie/Map Generation Data")]
+[CreateAssetMenu(fileName = "MapGenerationData", menuName = "ThriveOrDie/Map Generation Data")]
 public class MapGeneratorData : ScriptableObject
 {
   /// <summary>The map size. Consider puting this in the Game manager instead?</summary>
