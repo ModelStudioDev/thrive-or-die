7c75490983a2f309e4d883df3d117032e616af37
Nil <nilbadia@gmail.com>
2026-01-30 22:42:31 +0100

    added another UI document, added code to the building menu main button, added sound to the onClick function, added animation(doesnt work)

---
+++ Assets/Scripts/UI_Scripts/BuildingMenuEvents.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BuildingMenuEvents : MonoBehaviour
+{
+  /// <summary>
+  /// The UI document of the building menu main button
+  /// </summary>
+  private UIDocument _document;
+  [SerializeField] UIDocument buildingMenu_Document;
+  /// <summary>
+  /// The button
+  /// </summary>
+  private Button button;
+  private AudioSource buttonSound;
+
+  private void Awake()
+  {
+    _document = GetComponent<UIDocument>();
+    buttonSound = GetComponent<AudioSource>();
+    button = _document.rootVisualElement.Q("BuildingMenuButton") as Button;
+    button.RegisterCallback<ClickEvent>(OnBuildMenuClick);
+    buildingMenu_Document.enabled = false;
+  }
+
+  private void OnDisable()
+  {
+    button.UnregisterCallback<ClickEvent>(OnBuildMenuClick);
+  }
+
+  private void OnBuildMenuClick(ClickEvent evt) {
+    Debug.Log("Building Menu clicked");
+    buildingMenu_Document.enabled = true;
+    buttonSound.Play();
+  }
+
+}
