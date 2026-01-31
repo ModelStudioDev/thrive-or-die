87fc4a532f434c995e5dd00cbaad7f132fc0ad49
Bryteep <139593534+IsaacCastellvi@users.noreply.github.com>
2026-01-31 18:51:49 +0100

    fixed structure data

--- Assets/Scripts/BuildingData/StructureData.cs
+++ Assets/Scripts/BuildingData/StructureData.cs
@@ -3,14 +3,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BaseBuilding", menuName = "Scriptable Objects/BaseBuilding")]
 public class StructureData : ScriptableObject
 {
-
-    [SerializeField]
     public Vector3 Cords = Vector3.zero;
     
-    [System.Serializable]
-
     /// <summary>Sets the current state of the structure</summary>
-    public enum StructureState
+    enum StructureState
     {
         Blueprint,
         Constructing, 
@@ -18,12 +14,28 @@ public class StructureData : ScriptableObject
         Damaged, 
         Destroyed,
     }
+    enum StructureType
+    {
+        //Does Nothing,default if not indicated
+        Base = 0,
+        Storage,
+        Crafting,
+        Defense,
+        Walls,
+        Decoration,
+        Investigation,
+        //add more into future development
+    }
 
     public StructureState State;
-
+    public StructureState Type;
     public string BuildingName = "Base";
     public int Health = 100;
-    public int MaxHealth;
+    public int Size = 1;
+    int MaxLevel;
+    
+    /// <summary>Used to multiply it to the level to bost stats</summary>
+    public float LevelMultiplier = 1.2f;
 
     /// <summary>Stores all sprites of a buildig in a list</summary>
     public Sprite[] Sprite;
