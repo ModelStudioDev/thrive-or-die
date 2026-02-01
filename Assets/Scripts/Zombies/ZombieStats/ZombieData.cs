81dee71eca505925578edf80adfd429754ff32fa
Nil <nilbadia@gmail.com>
2026-02-01 18:04:03 +0100

    removed strength variables and added more specific ones

--- Assets/Scripts/Zombies/ZombieStats/ZombieData.cs
+++ Assets/Scripts/Zombies/ZombieStats/ZombieData.cs
@@ -6,13 +6,25 @@ namespace ThriveOrDie.Zombies
   public class ZombieData : ScriptableObject
   {
     /// <summary>
+    /// The distance at wich a given zombie can spot humans
+    /// </summary>
+    public float spottingRange;
+    /// <summary>
+    /// The atack speed of a given zombie
+    /// </summary>
+    public float atackSpeed;
+    /// <summary>
+    /// The damage per hit value
+    /// </summary>
+    public float damagePerHit;
+    /// <summary>
+    /// The resistence of a given zombie to atacks aimed at him
+    /// </summary>
+    public float atackResistence;
+    /// <summary>
     /// The max health of a zombie
     /// </summary>
     public float maxHealth;
-    /// <summary>
-    /// The strength value of a zombie
-    /// </summary>
-    public float strength;
     /// <summary>
     /// The zombie speed
     /// </summary>
