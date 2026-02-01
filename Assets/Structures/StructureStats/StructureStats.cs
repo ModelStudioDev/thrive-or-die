e28b76ec49b3040e2deb0c52942fc03cdca22542
Bryteep <139593534+IsaacCastellvi@users.noreply.github.com>
2026-02-01 19:18:55 +0100

    Resolved [https://github.com/ModelStudioDev/thrive-or-die/pull/33#discussion_r2751255684] and [https://github.com/ModelStudioDev/thrive-or-die/pull/33#discussion_r2751256112]

--- Assets/Structures/StructureStats/StructureStats.cs
+++ Assets/Structures/StructureStats/StructureStats.cs
@@ -8,9 +8,6 @@ namespace ThriveOrDie.Structures
         public int Health {get; set;}
         public Vector3 Pos {get; set;}
         public int Level {get; set;}
-        public int XP {get; set;}
-        /// <summary>Progress if the building is in construction</summary>
-        public float BuildingProgress {get; set;}
         public StructureState CurrentState {get; set;}
     }
 }
