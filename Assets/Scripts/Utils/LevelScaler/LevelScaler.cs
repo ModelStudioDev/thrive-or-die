98f7ac35fb3a548b65d8fd8b09ffb2888f8e9b7d
Adise <admin@pauibanez.me>
2026-02-02 11:04:41 +0100

    created base levelScalar editor

--- Assets/Scripts/Utils/LevelScaler/LevelScaler.cs
+++ Assets/Scripts/Utils/LevelScaler/LevelScaler.cs
@@ -12,7 +12,7 @@ namespace ThriveOrDie.Utils
     Scaled
   }
 
-
+  [Serializable]
   public record LevelScaler<DataType>
   {
     #region Data
@@ -48,11 +48,11 @@ namespace ThriveOrDie.Utils
         case ScalerType.Scaled:
           if (currentLevel == -1) throw new Exception("Can't get scaled value with no currentLevel");
           if (!IsNumber(typeof(DataType))) throw new Exception("Can't set scalar on non-number type");
-          try { return (dynamic)baseValue * (multiplyer * currentLevel); }
+          try { return Scale(baseValue, multiplyer * currentLevel); }
           catch (Exception ex) { throw new Exception($"Operation failed for scalar: ex {ex.Message}"); }
 
         default:
-          throw new System.Exception($"Unknown scaling type {scalerType}");
+          throw new Exception($"Unknown scaling type {scalerType}");
       }
 
       #endregion
@@ -71,6 +71,33 @@ namespace ThriveOrDie.Utils
            type == typeof(float) ||
            type == typeof(double) ||
            type == typeof(decimal);
+      #endregion
+    }
+
+    /// <summary>Handles type-safe scalability operations</summary>
+    /// <param name="value">The value to scale</param>
+    /// <param name="scalar">The current scale factor</param>
+    /// <returns>The scaled value</returns>
+    /// <exception cref="Exception">Throws if the type is not implemented</exception>
+    private DataType Scale(DataType value, float scalar)
+    {
+      #region Scale
+      if (typeof(DataType) == typeof(int))
+        return (DataType)(object)((int)(object)value * scalar);
+
+      if (typeof(DataType) == typeof(float))
+        return (DataType)(object)((float)(object)value * scalar);
+
+      if (typeof(DataType) == typeof(double))
+        return (DataType)(object)((double)(object)value * scalar);
+
+      if (typeof(DataType) == typeof(long))
+        return (DataType)(object)((long)(object)value * scalar);
+
+      if (typeof(DataType) == typeof(short))
+        return (DataType)(object)(short)((short)(object)value * scalar);
+
+      throw new Exception($"Data type {typeof(DataType).Name} is not implemented");
       #endregion
     }
     #endregion
