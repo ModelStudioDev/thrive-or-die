2eb51cbc37cf2a9f03148adb81482e4474ba5028
Bryteep <139593534+IsaacCastellvi@users.noreply.github.com>
2026-02-02 18:18:39 +0100

    Resolved footprint matrix

--- Assets/Structures/StructureData/StructureData.cs
+++ Assets/Structures/StructureData/StructureData.cs
@@ -10,11 +10,17 @@ namespace ThriveOrDie.Structures
         /// <summary>Sets the current state of the structure</summary>
         public StructureType type;
         public string name;
+        //not implemented
         //LevelScaler<int> health = new();
         public int size;
         [SerializeField]
         public int maxLevel;
         /// <summary>Stores all sprites of a buildig in a list</summary>
         public Sprite[] sprite;
+        /*99% this is not your intended implementation,also i dont know what you mean making
+        it variable per level,could collision with other structure when lvl'ing up */
+        //can't initialize to size in an so
+        public bool[][] footprint;
+
     }
 }
