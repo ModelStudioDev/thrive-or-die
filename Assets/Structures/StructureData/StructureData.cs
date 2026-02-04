b295bf8c3dd69940f6a9c590b87220d6ab2b337d
Bryteep <139593534+IsaacCastellvi@users.noreply.github.com>
2026-02-04 21:35:55 +0100

    resolved StructureData

--- Assets/Structures/StructureData/StructureData.cs
+++ Assets/Structures/StructureData/StructureData.cs
@@ -1,23 +1,16 @@
 using UnityEngine;
-
+using ThriveOrDie.Utils;
 namespace ThriveOrDie.Structures
 {
     [CreateAssetMenu(fileName = "Structure", menuName = "ThriveOrDie/Structure")]
     public class StructureData : ScriptableObject
     {
-        /// <summary>Sets the current state of the structure</summary>
+        /// <summary>Specific type of the structure</summary>
         public StructureType type;
         public string name;
-        //not implemented
-        //LevelScaler<int> health = new();
+        LevelScaler health = new();
         public int size;
-        [SerializeField]
         public int maxLevel;
-        /// <summary>Stores all sprites of a buildig in a list</summary>
-        public Sprite[] sprite;
-        /*99% this is not your intended implementation,also i dont know what you mean making
-        it variable per level,could collision with other structure when lvl'ing up */
-        //can't initialize to size in an so
         public bool[][] footprint;
 
     }
