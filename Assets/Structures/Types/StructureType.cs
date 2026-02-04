6a90288daf9b6e4051edbf7390e3f0d601d9caa9
Bryteep <139593534+IsaacCastellvi@users.noreply.github.com>
2026-02-04 21:16:10 +0100

    resolved [https://github.com/ModelStudioDev/thrive-or-die/pull/33#discussion_r2760327888]

--- Assets/Structures/Types/StructureType.cs
+++ Assets/Structures/Types/StructureType.cs
@@ -5,12 +5,12 @@ namespace ThriveOrDie.Structures
     
     public enum StructureType
     {
-        none = 0,
-        storage,
-        crafting,
-        defense,
-        walls,
-        decoration,
-        investigation,
+        Storage,
+        Crafting,
+        Defense,
+        Walls,
+        Decoration,
+        Investigation,
+        Other
     }
 }
