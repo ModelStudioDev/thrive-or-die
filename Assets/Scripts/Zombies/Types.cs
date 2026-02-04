70742467647c09a231c39f43fc46c216d72e4b10
Nil <nilbadia@gmail.com>
2026-02-04 22:13:19 +0100

    removed level enum and made enum things upper case

--- Assets/Scripts/Zombies/Types.cs
+++ Assets/Scripts/Zombies/Types.cs
@@ -6,22 +6,11 @@ namespace ThriveOrDie.Zombies
   /// </summary>
   public enum ZombieType
   {
-    regular,
-    crawler,
-    agressive,
-    spitZombie,
-    tank,
+    REGULAR,
+    CRAWLER,
+    AGRESSIVE,
+    SPITZOMBIE,
+    TANK,
   }
 
-  /// <summary>
-  /// The level of a given zombie
-  /// </summary>
-  public enum ZombieLevel
-  {
-    lvl_1,
-    lvl_2,
-    lvl_3,
-    lvl_4,
-    lvl_5,
-  }
 }
